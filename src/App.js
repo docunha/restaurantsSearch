ba2a513defd4dc7383dc8b4a60b62469023bced4
Rodrigo Cunha <github@docunha.com.br>
2021-09-17 09:29:13 -0300

    Cria Home page e reset de estilos

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Home from './pages/Home';
+
+import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
+import theme from './theme';
+
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+    <Reset />
+    <Home />
+  </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable camelcase */
+import React, { useState } from 'react';
+
+
+
+import { Container, Search, Logo, Title, Carousel, Wrapper } from './styles';
+
+const Home = () => {
+
+
+  return (
+    <Wrapper>
+      <Container> <div>Hello</div>
+
+
+      </Container>
+    </Wrapper>
+  );
+};
+
+export default Home;
